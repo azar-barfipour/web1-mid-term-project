ea560f7044e800b693cd41d19ec382b6c5b1cb3f
Azar Barfipour <azar.barfipour@gmail.com>
2021-12-16 14:33:37 -0800

    done

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let button = document.querySelector('.button');
+let button = document.querySelector('button');
 let ul = document.querySelector('ul');
 let div = document.querySelector('.backdrop');
 

219006d41f89a213bb89f6ad901f3db2c49951c2
Azar Barfipour <azar.barfipour@gmail.com>
2021-12-16 12:17:30 -0800

    add js for nav button and backdrop

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-let button = document.querySelector('button');
+let button = document.querySelector('.button');
 let ul = document.querySelector('ul');
-
+let div = document.querySelector('.backdrop');
 
 button.addEventListener('click',function(){
     ul.classList.add('nav__list--show');
+    div.classList.add('show');
+})
+
+div.addEventListener('click',function(){
+    ul.classList.remove('nav__list--show');
+    div.classList.remove('show');
 })
